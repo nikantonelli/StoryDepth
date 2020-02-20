399cd23a67b07a57d858fc7b8e6b60d098b6162b
Nik <nik@nikantonelli.com>
2020-02-20 16:40:58 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,82 @@
+Ext.define('Niks.Apps.StoryDepth', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+
+    items: [
+        {
+            xtype: 'container',
+            itemId: 'option_box',
+            layout: 'hbox'
+        }
+    ],
+
+    html: '',
+
+    _getData: function(query) {
+        var me = this;
+        var totals = Ext.create('Deft.Deferred');
+        this._loadRecordCount('hierarchicalrequirement', Rally.data.wsapi.Filter.fromQueryString('('+query+' >0)'), 'ans').then({
+            success: function(result) {
+                    totals.resolve(result);
+            },
+            failure: function(result) {
+                console.log('Failed: ', result);
+                totals.reject(result);
+            }
+        });
+                return totals.promise;
+    },
+
+    _recurse: function(query) {
+        var me = this;
+        this._getData(query).then({
+            success: function(results) {
+                if (results.ans !==0) {
+                    me.add( {
+                        xtype: 'container',
+                        html: 'Level: ' + query.match(/Parent/g).length + ' Count: ' + results.ans
+                    });
+
+                    me._recurse('Parent.'+query);
+                }
+            },
+            failure: function(result) {
+                debugger;
+            },
+            scope: me
+        })
+    },
+
+    launch: function() {
+        var qStr = 'Parent.PortfolioItem.ObjectID';
+        this._recurse(qStr);
+    },
+
+    _loadRecordCount: function(model, filters, id){
+        var deferred = Ext.create('Deft.Deferred');
+        var me = this;
+        console.log("Starting load: model>>",model, 'filters>>', filters.toString());
+
+        Ext.create('Rally.data.wsapi.Store', {
+            model: model,
+            filters: filters,
+            limit: 1,
+            pageSize: 1
+        }).load({
+            callback : function(records, operation, successful) {
+                var result = {};
+                if (successful){
+                    console.log('result:', operation);
+                    result[id] = operation.resultSet.totalRecords || 0;
+                    deferred.resolve(result);
+                } else {
+                    console.log("Failed: ", operation);
+                    result[id] = 0;
+                    deferred.resolve(result);
+                    //deferred.reject("Couldn't Load: " + operation.error.errors.join('. '));
+                }
+            }
+        });
+        return deferred.promise;
+    }
+});
---
+++ README.md
@@ -0,0 +1,14 @@
+Parentage-Trend
+=========================
+
+## Overview
+
+Count how many stories there are as children of children of chidren etc. I.e. the hierarchy of UserStory under UserStory, ad nauseam.
+
+## License
+
+Parentage-Trend is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
+
+##Documentation for SDK
+
+You can find the documentation on our help [site.](https://help.rallydev.com/apps/2.1/doc/)
